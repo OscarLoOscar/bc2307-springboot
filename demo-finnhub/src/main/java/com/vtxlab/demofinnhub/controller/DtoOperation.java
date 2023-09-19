1c0c0bf4650f6de0ef88ccd6e107fa1728673277
OscarLoOscar <lokongkitoscar@gmail.com>
2023-09-19 16:27:09 +0800

    updated

--- demo-finnhub/src/main/java/com/vtxlab/demofinnhub/controller/DtoOperation.java
+++ demo-finnhub/src/main/java/com/vtxlab/demofinnhub/controller/DtoOperation.java
@@ -3,12 +3,14 @@ package com.vtxlab.demofinnhub.controller;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.vtxlab.demofinnhub.infra.ApiResponse;
 import com.vtxlab.demofinnhub.model.quoteReqDto;
 import com.vtxlab.demofinnhub.model.Resp.totalRespSto;
 
 public interface DtoOperation {
   @GetMapping(value = "/stock")
-  ResponseEntity<ApiResponse<totalRespSto>> getStock(String symbol);
+  ResponseEntity<ApiResponse<totalRespSto>> getStock(
+      @RequestParam(value = "symbol") String symbol);
 
 }
--- demo-finnhub/src/main/java/com/vtxlab/demofinnhub/controller/impl/DtoController.java
+++ demo-finnhub/src/main/java/com/vtxlab/demofinnhub/controller/impl/DtoController.java
@@ -1,42 +1,43 @@
-// package com.vtxlab.demofinnhub.controller.impl;
+package com.vtxlab.demofinnhub.controller.impl;
 
-// import java.util.List;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import com.vtxlab.demofinnhub.controller.DtoOperation;
-// import com.vtxlab.demofinnhub.infra.ApiResponse;
-// import com.vtxlab.demofinnhub.model.CompanyRequestDto;
-// import com.vtxlab.demofinnhub.model.quoteReqDto;
-// import com.vtxlab.demofinnhub.model.Resp.totalRespSto;
-// import com.vtxlab.demofinnhub.services.companyService;
-// import com.vtxlab.demofinnhub.services.quoteService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.vtxlab.demofinnhub.controller.DtoOperation;
+import com.vtxlab.demofinnhub.infra.ApiResponse;
+import com.vtxlab.demofinnhub.mapper.Mapper;
+import com.vtxlab.demofinnhub.model.CompanyRequestDto;
+import com.vtxlab.demofinnhub.model.quoteReqDto;
+import com.vtxlab.demofinnhub.model.Resp.totalRespSto;
+import com.vtxlab.demofinnhub.services.companyService;
+import com.vtxlab.demofinnhub.services.quoteService;
 
-// @RestController
-// @RequestMapping(value = "/api/v1")
-// public class DtoController implements DtoOperation {
+@RestController
+@RequestMapping(value = "/api/v1")
+public class DtoController implements DtoOperation {
 
-//   @Autowired
-//   companyService CompanyService;
+  @Autowired
+  companyService CompanyService;
 
-//   @Autowired
-//   quoteService QuoteService;
+  @Autowired
+  quoteService QuoteService;
 
-//   @Override
-//   public ResponseEntity<ApiResponse<totalRespSto>> getStock(String symbol) {
-//     List<quoteReqDto> conventQuote = QuoteService.getCompanyPrice(symbol);
-//     List<CompanyRequestDto> conventCompany =
-//         CompanyService.getCompanyData(symbol);
+  @Override
+  public ResponseEntity<ApiResponse<totalRespSto>> getStock(String symbol) {
+    CompanyRequestDto conventCompany =
+        CompanyService.getCompanyData(symbol).get(0);
 
+    quoteReqDto conventQuote = QuoteService.getCompanyPrice(symbol).get(0);
+    totalRespSto output = Mapper.map(conventCompany, conventQuote);
 
-//     ApiResponse<List<totalRespSto>> response =
-//         ApiResponse.<List<totalRespSto>>builder()//
-//             .ok()//
-//             .data(convent)//
-//             .build();
+    ApiResponse<totalRespSto> response = ApiResponse.<totalRespSto>builder()//
+        .ok()//
+        .data(output)//
+        .build();
 
-//     return ResponseEntity.ok().body(response);
-//   }
+    return ResponseEntity.ok().body(response);
+  }
 
-// }
+}
