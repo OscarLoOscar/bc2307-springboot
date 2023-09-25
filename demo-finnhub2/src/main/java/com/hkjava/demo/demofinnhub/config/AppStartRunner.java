1698099063c5b974ee72b6fa008993003849066d
OscarLoOscar <lokongkitoscar@gmail.com>
2023-09-25 10:55:50 +0800

    updated

--- demo-finnhub2/src/main/java/com/hkjava/demo/demofinnhub/config/AppStartRunner.java
+++ demo-finnhub2/src/main/java/com/hkjava/demo/demofinnhub/config/AppStartRunner.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import com.hkjava.demo.demofinnhub.entity.Stock;
 import com.hkjava.demo.demofinnhub.entity.StockPrice;
@@ -110,4 +111,5 @@ public class AppStartRunner implements CommandLineRunner {
     log.info("CommandLineRunner Completed");
 
   }
+
 }
