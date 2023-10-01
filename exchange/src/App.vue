8613986b17492217ca1134ade2ab392659940626
OscarLoOscar <lokongkitoscar@gmail.com>
2023-10-01 16:31:45 +0800

    updated

--- exchange/src/App.vue
+++ exchange/src/App.vue
@@ -10,8 +10,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {//記住template 入面都要有
+    HelloWorld,
   }
 }
 </script>
